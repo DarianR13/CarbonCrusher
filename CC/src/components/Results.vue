3ab52cbc17c5733395db3f1878c3cf27d900bfbd
Kali <kapdayalexis@gmail.com>
2024-04-25 14:24:35 -0400

    Add buttons

--- CC/src/components/Results.vue
+++ CC/src/components/Results.vue
@@ -3,6 +3,22 @@
         <h1>Results</h1>
     <router-link to="/calculator">Try Again?</router-link>
     </v-container>
+    <v-container>
+      <!-- I'm not sure that we need these buttons other than delete since Submitting would both add and update your current score automattically-->
+    <v-row align="center" justify="center">
+      <v-col cols="auto">
+        <v-btn density="compact">Add</v-btn>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn density="comfortable">Delete</v-btn>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn density="default">Update</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
     
    <footer class="bg-grey-lighten-1 footer">footer</footer>
 </template>
